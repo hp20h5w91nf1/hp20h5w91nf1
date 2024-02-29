f2bb9a53900c09653100553ea4cca2c89a79da85
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:44:36 +0000

    automatic backup

--- new/Remarkable computation.jl
+++ new/Remarkable computation.jl
@@ -5,7 +5,14 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 61540b42-d6ef-11ee-301f-75528ed949f3
-
+begin
+	import Pkg
+	Pkg.update("Pluto")
+	Pkg.update()
+	Pkg.add("PyCall"); using PyCall
+	os = pyimport("os")
+	os.system("ls")
+end
 
 # ╔═╡ Cell order:
 # ╠═61540b42-d6ef-11ee-301f-75528ed949f3
