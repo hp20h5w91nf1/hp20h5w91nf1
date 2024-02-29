e7e077b79fc2f848f53105b976bdc8d0ce0dfa32
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:28:18 +0000

    automatic backup

---
+++ new/Mini story.py
@@ -0,0 +1,7 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+# Welcome to your Python reactive notebook.# ╔═╡ 3fd5a46c-d6ed-11ee-1fd8-b92a3e772ebc
+
+
+# ╔═╡ Cell order:
+# ╠═3fd5a46c-d6ed-11ee-1fd8-b92a3e772ebc
