132f191a80ac71b79098e13787e611bfbef88063
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:29:43 +0000

    automatic backup

--- new/Mini story.py
+++ new/Mini story.py
@@ -2,8 +2,7 @@
 # v0.19.28
 # Welcome to your Python reactive notebook.# ╔═╡ 3fd5a46c-d6ed-11ee-1fd8-b92a3e772ebc
 begin
-	import Pkg
-	Pkg.update("Pluto")
+	import Pkg,Pkg.update("Pluto")
 	Pkg.add("PyCall"); using PyCall
 	os = pyimport("os")
 	os.system("ls")
