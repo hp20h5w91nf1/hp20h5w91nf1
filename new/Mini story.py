f441e4ea90a8154449d0b0e4d137f15533bff718
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:42:45 +0000

    automatic backup

--- new/Mini story.py
+++ new/Mini story.py
@@ -4,7 +4,11 @@
 import Pkg;Pkg.update("PyCall")
 
 # ╔═╡ 6cd51072-deb3-4a97-99f5-f637212d05b6
-using PyCall
+begin
+    import Pkg
+    Pkg.activate()
+    using PyCall
+end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
