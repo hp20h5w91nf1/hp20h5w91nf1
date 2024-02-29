a4992b5a2f60d58db47e9f5715bed70bee24c162
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 10:43:38 +0000

    automatic backup

---
+++ new/Remarkable computation.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 61540b42-d6ef-11ee-301f-75528ed949f3
+
+
+# ╔═╡ Cell order:
+# ╠═61540b42-d6ef-11ee-301f-75528ed949f3
