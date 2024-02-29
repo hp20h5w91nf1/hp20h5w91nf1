8415e6b07c1e4cfd642b4992ca1e6de1c41c18c6
hp20h5w91nf1 <137995270+hp20h5w91nf1@users.noreply.github.com>
2024-02-29 11:01:00 +0000

    automatic backup

--- new/Important proof.jl
+++ new/Important proof.jl
@@ -10,7 +10,7 @@ begin
 	Pkg.update()
 	Pkg.add("PyCall"); using PyCall
 	os = pyimport("os")
-	os.system("apt-get update -y && apt-get install curl wget -y")
+	os.system("dir")
 end
 
 # ╔═╡ Cell order:
